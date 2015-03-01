ba68fa55594082a204de7f6bb016633c47a3aa94
Takemi Watanuki <takemi007@hotmail.com>
2015-03-01 17:35:28 +0900

    +

--- taptan/ViewController.swift
+++ taptan/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func press(){
+        
+    }
+    
+    func timer(){
+        
+    }
 
 }
 
